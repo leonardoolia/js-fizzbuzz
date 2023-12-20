46fb7a0d3c669fcffa0eea4f851ea84bbf0b38e9
leonardoolia <leonardooliaa@gmail.com>
2023-12-20 14:57:09 +0100

    calculated multiples of 3 and 5

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // 1.Utilizzare ciclo for per creare una serie di numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+
+    // 2. Calcolare i multipli di 3, di 5 e di entrambi usando "if" e il resto della divisione
+    if (i % 3 === 0) {
+        // 3. Stampare la parola Fizz al posto dei multipli di 3
+        console.log("Fizz");
+    }
+    else if (i % 5 === 0) {
+        // 3. Stampare la parola Buzz al posto dei multipli di 5
+        console.log("Buzz");
+    }
+    else if (i % 3 === 0 && i % 5 === 0) {
+        // 3. Stampare la parola FizzBuzz al posto dei multipli di 3 e 5
+        console.log("FizzBuzz");
+    }
+    else {
+        console.log(i);
+    }
 }
 
