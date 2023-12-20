48ea741a03dd60c5033d9329fb68038ea221cc27
leonardoolia <leonardooliaa@gmail.com>
2023-12-20 15:13:52 +0100

    fixed FizzBuzz for multiples of 3 and 5

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@
 for (let i = 1; i <= 100; i++) {
 
     // 2. Calcolare i multipli di 3, di 5 e di entrambi usando "if" e il resto della divisione
-    if (i % 3 === 0) {
-        // 3. Stampare la parola Fizz al posto dei multipli di 3
-        console.log("Fizz");
+    if (i % 3 === 0 && i % 5 === 0) {
+        // 3. Stampare la parola FizzBuzz al posto dei multipli di 3 e 5
+        console.log("FizzBuzz");
     }
     else if (i % 5 === 0) {
         // 3. Stampare la parola Buzz al posto dei multipli di 5
         console.log("Buzz");
     }
-    else if (i % 3 === 0 && i % 5 === 0) {
-        // 3. Stampare la parola FizzBuzz al posto dei multipli di 3 e 5
-        console.log("FizzBuzz");
+    else if (i % 3 === 0) {
+        // 3. Stampare la parola Fizz al posto dei multipli di 3
+        console.log("Fizz");
     }
     else {
         console.log(i);
